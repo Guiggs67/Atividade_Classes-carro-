ce301be295483bd1d0dec9af98789c7e24d5cc36
Guilherme Augusto <116207265+Guiggs67@users.noreply.github.com>
2023-03-13 20:16:28 -0300

    Add files via upload

---
+++ Atividade de classe(carro)/Carro.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_de_classe_carro_
+{
+    internal class Carro
+    {
+        public string _modelo;
+        public string _marca;
+        public int _ano;
+        public double _velocidade;
+       
+        public Carro(string  modelo, string marca, int ano, double velocidade)
+        {
+            _modelo = modelo;
+            _marca = marca;
+            _ano = ano;
+            _velocidade = velocidade;
+        }
+        public void Acelerar()
+        {
+            _velocidade +=50;
+                 
+
+        }
+        public void Parar()
+        {
+            _velocidade =0;
+        }
+
+        public void Escrita()
+        {
+            Console.WriteLine($"_modelo: {_modelo}");
+            Console.WriteLine($"_marca: {_marca}");
+            Console.WriteLine($"_modelo: {_ano}");
+            Console.WriteLine($"_: {_velocidade}");
+
+        }
+
+
+    }
+
+    
+        
+        
+}
---
+++ Atividade de classe(carro)/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_de_classe_carro_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n_carros;
+            Console.WriteLine("Digite o numero de carros: ");
+            n_carros = int.Parse(Console.ReadLine());
+
+            Carro[] C1 = new Carro[n_carros];
+
+            string marca;
+            string modelo;
+            int ano;
+            double velocidade;
+            
+
+
+            for(int i =0; i <n_carros; i ++)
+            {
+                Console.WriteLine("Digite a marca do carro:");
+                marca = Console.ReadLine();
+                Console.WriteLine("Digite a modelo do carro:");
+                modelo= Console.ReadLine();
+                Console.WriteLine("Digite a ano do carro:");
+                ano= int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite a velocidade do carro:");
+                velocidade= double.Parse(Console.ReadLine());
+                C1[i] = new Carro(modelo, marca, ano, velocidade);
+
+                Console.WriteLine("Deseja acelerar(1) ou parar(2)");
+                int resposta = int.Parse(Console.ReadLine());
+
+                if (resposta == 1)
+                {
+                    C1[i].Acelerar();
+                }
+                else
+                {
+                    C1[i].Parar();
+                }
+
+            }
+            for (int i = 0; i < n_carros; i++)
+            {
+                C1[i].Escrita();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
